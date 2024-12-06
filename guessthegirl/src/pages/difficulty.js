3ba48e9102c6395daea2b65e9e9a9026988f1402
LauraKirsty <laurakirsty12@gmail.com>
2024-12-06 21:53:19 +0000

    Commit updates for game-page routing

--- guessthegirl/src/pages/difficulty.js
+++ guessthegirl/src/pages/difficulty.js
@@ -2,15 +2,21 @@ import React from 'react';
 import './difficulty.css'; // Import the CSS file for styling
 import Footer from '../components/footer';
 import NavigationBar from '../components/NavBar';
+import { useNavigate } from "react-router-dom";  // Importing useNavigate for navigation
 
 const DifficultyPage = () => {
+  const navigate =useNavigate();
+
+  const goToEasyGame =() => {
+    navigate("/easygame");
+  };
 return (
 <div>
 <NavigationBar />
 <div className="app_header">Select <br></br>difficulty level</div>
 <div className="DifficultyContainer"> 
   <div className ="DifficultyLevel">
-  <button className="difficulty-button">Easy</button> 
+  <button className="difficulty-button" onClick={goToEasyGame}>Easy</button> 
   <p>10 guesses</p> 
 </div> 
   <div className ="DifficultyLevel">
