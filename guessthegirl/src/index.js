265b5708af5de3cd36a62fbbb0601eef2a7c8303
LauraKirsty <laurakirsty12@gmail.com>
2024-12-08 00:55:33 +0000

    Updated nav-bar links and CSS

--- guessthegirl/src/index.js
+++ guessthegirl/src/index.js
@@ -7,22 +7,23 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Creators from './pages/about';
 import DifficultyPage from './pages/difficulty';
-import ConstructionPage from './components/construction';
 import SuggestionPage from './pages/suggestion';
 import FeedbackPage from './pages/feedback';
 import StatsPage from './pages/stats';
+import HowToPage from './pages/howtoplay';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <Router>
     <Routes>
-      <Route path="/" element={<App />} />         {/* The Home page */}
+      <Route path="/" element={<App />} /> {/* The Home page */}
       <Route path="/difficulty" element={<DifficultyPage />} />  {/* The Difficulty page */}
       <Route path="/about" element={<Creators />} /> {/*The About page */}
       <Route path="/easygame" element={<Game />} /> {/*The easy game page */}
       <Route path="/stats" element={<StatsPage />} /> {/* The stats page */}
       <Route path="/suggestion" element={<SuggestionPage />} /> {/*The suggest an artist page */}
       <Route path="/feedback" element={<FeedbackPage />} /> {/*The feedback page */}
+      <Route path="/howtoplay" element={<HowToPage />} /> {/*The how to page */}
           </Routes>
   </Router>
 );
