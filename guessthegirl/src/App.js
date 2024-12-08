ac03e50ecf6fd19e98a59f1ccde0ac504870571d
SrutiSaraswatula <srutisaraswatula@gmail.com>
2024-12-08 14:28:37 +0000

    Update App.js comments

--- guessthegirl/src/App.js
+++ guessthegirl/src/App.js
@@ -1,11 +1,14 @@
+// Import Statements
 import React from 'react';
 import './App.css'; 
 import { useNavigate } from "react-router-dom";  
 import Footer from './components/footer';
 import NavigationBar from './components/NavBar';
 
-
+// App.js function
 const App = () => {
+
+  // Routing functions
   const navigate = useNavigate();  
 
   const goToDifficultyPage = () => {
@@ -15,15 +18,16 @@ const App = () => {
   const goToHowToPlayPage = () => {
     navigate("/howtoplay"); 
   };
-  
+
+  // Return statement
   return (
     <div>
       <NavigationBar />
-    <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
       <div className="app_header">Guess the Girl</div>
 
       <div className="app_font">
@@ -31,7 +35,7 @@ const App = () => {
         favourite female artists
       </div>
 
-
+      // 'Play' and 'How to Play' buttons
       <button className="play_button" onClick={goToDifficultyPage}>Play</button>
       <button className="how_to_button" onClick={goToHowToPlayPage}>How to Play</button>
 
